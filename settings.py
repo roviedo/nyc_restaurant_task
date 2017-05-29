d7558c65f28a4ce88417a94c3a1c359c816e86e5
Raul Oviedo <antoniorah@gmail.com>
2017-05-29 19:09:14 -0400

    Move hardcoded variables to local_settings file

--- settings.py
+++ settings.py
@@ -1,9 +1,12 @@
 PROD_POSTRGRES = {
-    "host": "ec2-54-235-120-39.compute-1.amazonaws.com",
-    "port": "5432",
-    "dbname": "dgjdd8htdubki",
-    "user": "ljbzjrxfwfgmmy",
-    "password": "fc38977e1a0e2a79e49af80435e2706d9c39947dbc3c9b96bd0d6e51bcef16bb"
+    "host": "test",
+    "port": "5000",
+    "dbname": "postgres",
+    "user": "user",
+    "password": "password"
 }
 
-REDIS_URL = 'redis://redis-rugged-3359'
+REDIS_URL = 'redis://localhost:6379'
+
+# Local settings overrides the above plus any other local variables
+from local_settings import *
